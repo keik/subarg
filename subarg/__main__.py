a98c08ece08db28916a9ac4358a3a46e884560c5
keik <k4t0.kei@gmail.com>
2016-08-14 13:59:01 +0900

    Implement

--- subarg/__main__.py
+++ subarg/__main__.py
@@ -1,5 +1,25 @@
 # -*- coding: utf-8 -*-
 
 
-def parse(argv):
-    pass
+def parse(argv, level=0):
+    nargs = []
+
+    for i in range(len(argv)):
+
+        if argv[i] == '[':
+            level += 1
+
+            if level == 1:
+                index = i + 1
+
+        elif argv[i] == ']':
+            level -= 1
+            sub = argv[index:i]
+
+            if level == 0:
+                nargs.append(parse(sub, level))
+
+        elif level == 0:
+            nargs.append(argv[i])
+
+    return nargs
--- tests/test_subarg.py
+++ tests/test_subarg.py
@@ -3,5 +3,46 @@
 from subarg import parse
 
 
-def test_here():
-    assert False
+def test_parse_no_subarg():
+    args = parse('-a b --c d -e'.split())
+    assert args == ['-a', 'b',
+                    '--c', 'd',
+                    '-e']
+
+
+def test_parse_1l_subarg():
+    args = parse('-a b --c [ d -e f ] -g'.split())
+    assert args == ['-a', 'b',
+                    '--c', ['d', '-e', 'f'],
+                    '-g']
+
+
+def test_parse_multiple_1l_subarg():
+    args = parse('-a b --c [ d -e f ] -g [ h -i j ] -k'.split())
+    assert args == ['-a', 'b',
+                    '--c', ['d', '-e', 'f'],
+                    '-g', ['h', '-i', 'j'],
+                    '-k']
+
+
+def test_parse_2l_subarg():
+    args = parse('-a b --c [ d -e [ f -g h ] i ] -j'.split())
+    print(args)
+    assert args == ['-a', 'b',
+                    '--c', ['d',
+                            '-e', ['f', '-g', 'h'],
+                            'i'],
+                    '-j']
+
+
+def test_parse_multiple_2l_subarg():
+    args = parse('-a b --c [ d -e [ f -g h ] i ] -j [ k -l [ m -n o ] p ] q'.split())
+    print(args)
+    assert args == ['-a', 'b',
+                    '--c', ['d',
+                            '-e', ['f', '-g', 'h'],
+                            'i'],
+                    '-j', ['k',
+                           '-l', ['m', '-n', 'o'],
+                           'p'],
+                    'q']
